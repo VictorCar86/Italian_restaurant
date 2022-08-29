d923212c03e1b77610b85b45e51b7a2e250d1000
Victor Carreno <vmanuelcar@hotmail.com>
2022-08-29 12:49:58 -0500

    complete responsive gallery container and create form components

--- src/containers/TasteGalleryPage/index.jsx
+++ src/containers/TasteGalleryPage/index.jsx
@@ -7,6 +7,12 @@ import ArticleInfo from '../../components/ArticleInfo'
 import tasteGalleryBigImg from '@gallery/taste_gallery-big.jpg'
 import mainPlateBigImg from '@gallery/main-plate-big.jpg'
 import mainPlateTinyImg from '@gallery/main-plate.jpg'
+import gallery1Img from '@gallery/gallery-1.jpg'
+import gallery2Img from '@gallery/gallery-2.jpg'
+import gallery3Img from '@gallery/gallery-3.jpg'
+import gallery4Img from '@gallery/gallery-4.jpg'
+import gallery5Img from '@gallery/gallery-5.jpg'
+import gallery6Img from '@gallery/gallery-6.jpg'
 import tasteGalleryArtBigImg from '@articles/article-taste-gallery-big.jpg'
 
 const TasteGalleryPage = () => {
@@ -23,37 +29,37 @@ const TasteGalleryPage = () => {
           <div className='TasteGalleryPage__main-plate__addon'></div>
         </picture>
         <section className='TasteGalleryPage__grid'>
-          <picture className=''>
+          <img className='TasteGalleryPage__grid__img' src={gallery1Img} alt="" />
+          {/* <picture className=''>
             <source srcSet={""} media="(max-width: 850px)" />
-            <img className='' src={""} alt="" />
-          </picture>
-          <picture className=''>
+          </picture> */}
+          <img className='TasteGalleryPage__grid__img' src={gallery2Img} alt="" />
+          {/* <picture className=''>
             <source srcSet={""} media="(max-width: 850px)" />
-            <img className='' src={""} alt="" />
-          </picture>
-          <picture className=''>
-            <source srcSet={""} media="(max-width: 850px)" />
-            <img className='' src={""} alt="" />
-          </picture>
+          </picture> */}
           <p>We are always happy to hear your suggestions and wishes!</p>
-          <picture className=''>
+          <img className='TasteGalleryPage__grid__img' src={gallery3Img} alt="" />
+          {/* <picture className=''>
             <source srcSet={""} media="(max-width: 850px)" />
-            <img className='' src={""} alt="" />
-          </picture>
+          </picture> */}
           <p>
             Our restaurant is glad to see our
             dear guests in the world of delicious
             and exquisite food, cooked
             according to the best Italian recipes
           </p>
-          <picture className=''>
+          <img className='TasteGalleryPage__grid__img' src={gallery4Img} alt="" />
+          {/* <picture className=''>
             <source srcSet={""} media="(max-width: 850px)" />
-            <img className='' src={""} alt="" />
-          </picture>
-          <picture className=''>
+          </picture> */}
+          <img className='TasteGalleryPage__grid__img' src={gallery5Img} alt="" />
+          {/* <picture className=''>
             <source srcSet={""} media="(max-width: 850px)" />
-            <img className='' src={""} alt="" />
-          </picture>
+          </picture> */}
+          <img className='TasteGalleryPage__grid__img' src={gallery6Img} alt="" />
+          {/* <picture className=''>
+            <source srcSet={""} media="(max-width: 850px)" />
+          </picture> */}
         </section>
         <ArticleInfo
           imgSrc={tasteGalleryArtBigImg}
