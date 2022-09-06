95436ced00a1e2f3451428d3025321dc2ee33faf
Victor Carreno <vmanuelcar@hotmail.com>
2022-09-06 13:21:17 -0500

    Complete responsive styles on MainPage

--- src/components/CongratsModal/index.jsx
+++ src/components/CongratsModal/index.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./CongratsModal.css"
 import ModalImg from '../../assets/images/addons/modal.jpg'
 import ReginaLogoImg from '../../assets/images/addons/la-regina-logo.png'
+import { useNavigate } from 'react-router-dom'
 
-const CongratsModal = ({ children }) => {
+const CongratsModal = ({ toggleState, children }) => {
+
+  const [modalClass, setModalClass] = useState("")
+
+  const navigator = useNavigate()
+
+  const closeModal = () => {
+    setModalClass("CongratsModal--OFF")
+    setTimeout(() => {
+        toggleState(prev => !prev)
+        navigator("/")
+    }, 600)
+  }
+
   return (
-    <div className='CongratsModal'>
-        <img className='CongratsModal__img' src={ModalImg} alt="La regina logo" />
-        <div className='CongratsModal__info'>
-            <img className='CongratsModal__info__img' src={ReginaLogoImg} alt="Background of blue neon waves" />
-            { children }
-        </div>
-    </div>
+    <section className={`${modalClass} CongratsModal`} onClick={closeModal} >
+      <div className='CongratsModal-container'>
+          <img className='CongratsModal-container__img' src={ModalImg} alt="La regina logo" />
+          <div className='CongratsModal-container__info'>
+              <img className='CongratsModal-container__info__img' src={ReginaLogoImg} alt="Background of blue neon waves" />
+              { children }
+          </div>
+      </div>
+    </section>
   )
 }
 
--- src/containers/ReservationFormPage/index.jsx
+++ src/containers/ReservationFormPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import "./ReservationFormPage.css"
 import Navbar from '../../components/Navbar'
 import SecondaryNav from '../../components/SecondaryNav'
@@ -10,6 +10,8 @@ import Modal from '../../components/Modal'
 import CongratsModal from '../../components/CongratsModal'
 
 const ReservationFormPage = () => {
+
+    const [modal, setModal] = useState(false)
 
     const pages = [
         {
@@ -24,6 +26,31 @@ const ReservationFormPage = () => {
 
     const { state, setState } = useContext(AppContext)
 
+    const form = useRef(null)
+
+    const updateState = () => {
+        const formData = new FormData(form.current)
+
+        if (formData.get("text") !== "" &&
+            formData.get("email") !== "" &&
+            formData.get("phone") !== "" &&
+            formData.get("date") !== ""
+          ){
+          setState({
+            ...state,
+            name: formData.get("text"),
+            email: formData.get("email"),
+            phone: formData.get("phone"),
+            birthday: formData.get("date"),
+          })
+          setModal(!modal)
+        }
+        else {
+          alert("Complete the entire form")
+        }
+        console.log(Object.fromEntries(formData), state)
+    }
+
     return (
       <>
           <Navbar />
@@ -33,28 +60,36 @@ const ReservationFormPage = () => {
               {state.date && (
                 <section className='ReservationFormPage__section'>
                     <h3 className='Title-reservation-pages'>Every element is required</h3>
-                    <form action="#" onSubmit={event => event.preventDefault()}>
+                    <form action="#" ref={form} onSubmit={event => event.preventDefault()}>
                       <div className='ReservationFormPage__section__grid'>
-                        <input type="text" placeholder='name' />
-                        <input type="email" placeholder='e-mail' name="" id="" />
-                        <input type="tel" placeholder='phone' name="" id="" />
-                        <input type="date" name="" id="" />
+                        <input type="text" placeholder='name' name='text' />
+                        <input type="email" placeholder='e-mail' name="email" />
+                        <input type="tel" placeholder='phone' name="phone" />
+                        <input
+                          type="text"
+                          name="date"
+                          placeholder='birthday'
+                          onFocus={prev => prev.target.type="date"}
+                          onBlur={prev => prev.target.type="text"}
+                        />
                       </div>
                       <h3 className='ReservationFormPage__section__summary'>Summary</h3>
                       <h3 className='ReservationFormPage__section__expected'>Expect {state.guests} guests on {state.date} at {state.hour} {state.time}</h3>
-                      <GenericButton text='Submit' />
+                      <GenericButton text='Submit' onClick={updateState} />
                     </form>
                 </section>
               )}
               {!state.date && <GoBackSection />}
             </ReservationSection>
           </main>
-          <Modal>
-            <CongratsModal>
-              <p>Thank you for your visit to us!</p>
-              <p>We look forward to hearing from you!</p>
-            </CongratsModal>
-          </Modal>
+          {modal && (
+            <Modal>
+              <CongratsModal toggleState={setModal} >
+                <p style={{fontSize: "2rem"}}>Thank you for your visit to us!</p>
+                <p style={{fontSize: "2rem"}}>We look forward to hearing from you!</p>
+              </CongratsModal>
+            </Modal>
+          )}
       </>
     )
 }
